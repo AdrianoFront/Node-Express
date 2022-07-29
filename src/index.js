40254752dee210491d1510e5666750f6a4fe0488
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-07-28 22:39:07 -0300

    Criado controller user e post

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-const express = require('express')
+import * as express from 'express'
+
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+import userController from './controller/userController'
+import postController from './controller/userController'
+
+app.use('/user', userController)
+app.use('/post', postController)
+
+
+app.post('/user/signup', (req, res) => {
+  res.send('SIGNUP /')
+})
+
+app.post('/user/login', (req, res) => {
+  res.send('LOGIN /')
+})
+
+app.post('/post', (req, res) => {
+  res.send('CREATE POST /')
+})
+
+app.get('/post/:id?', (req, res) => {
+  res.send('GET POST /')
 })
 
 app.listen(3000, () => console.log('ONLINE http://localhost:3000'))
